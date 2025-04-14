372bfff5cc10d94929e9bb8f2fe6e829c2d4dd80
Nisarg-Shah2302 <nisarg.shah.contractor@pepsico.com>
2025-04-14 16:20:42 +0530

    Clerk integration for user management and protecting routes

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -1,9 +1,16 @@
 "use client";
 import { useState } from "react";
+import { useAuth, useUser } from "@clerk/nextjs";
 
 export const Counter = () => {
     console.log(" COUNTER COMPONENT ")
     const [count, setCount] = useState(0);
+    // const { isLoaded, userId, sesseionId, getToken } = useAuth();
+    const { isLoaded, isSignedIn, user } = useUser();
+
+    if(!isLoaded || !isSignedIn) {
+        return null;
+    }
 
     return (
         <button
